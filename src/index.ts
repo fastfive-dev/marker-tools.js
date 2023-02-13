7687163fbbd9cb31d9f25ddda42993785bf7404b
Hyeonjong <nianelo4@gmail.com>
2023-02-13 19:36:54 +0900

    MarkerClustering 타입 export 추가

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ export function echo<T extends unknown>(value: T) {
   return value;
 }
 
-export { MarkerClusteringWrapper } from './MarkerClusteringWrapper';
+import { MarkerClusteringWrapper } from './MarkerClusteringWrapper';
+
+export { MarkerClusteringWrapper };
+
+export type MarkerClustering = MarkerClusteringWrapper['markerClustering'];
